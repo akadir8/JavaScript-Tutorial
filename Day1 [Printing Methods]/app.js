9a2b942e9ea98986c900b4524e43c8f33dec9995
akadir8 <bickiabdulkadir12@gmail.com>
2023-10-16 16:19:03 +0300

    İnit repo

---
+++ Day1 [Printing Methods]/app.js
@@ -0,0 +1,32 @@
+// Printing to the screen with document.write() and document.writeln()
+
+document.write("Hello World <br>");
+document.writeln("How are you <br>");
+document.write(7.4);
+
+
+// Printing to the console with console.log().
+
+console.log("Hello World");
+console.log("Nice to meet you");
+console.log(5,19);
+console.log(true);
+console.log({name:"John", surname:"Doe"});
+
+// Clearing with console.clear()
+
+console.clear();
+
+// By collecting two variables we can print them to the console
+
+let a = 5;
+let b = 10;
+
+console.log("Sum of two numbers :", a+b);
+console.clear();
+
+
+//Alert method
+//Warning message via pop-up window
+
+alert("Please do not press the F12 Key");
